31635db35b0d2826ccadc5dc69d10bab84706c89
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-11-16 12:49:51 +0100

    added app image

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -1,12 +1,19 @@
 import "./MainBanner.scss";
 
+const AquaSensApp = require("../../assets/images/AquaSensApp.png");
+
 const MainBanner = () => {
   return (
     <div className="main-banner">
       <div className="content">
-        <h1>AquaSens</h1>
-        <p>Quick insights into your aquarium stats</p>
-        <button>Buy Now</button>
+        <div className="sale-section">
+          <h1>AquaSens</h1>
+          <p>Quick insights into your aquarium stats</p>
+          <button>Buy Now</button>
+        </div>
+        <div className="app-image">
+          <img src={AquaSensApp} alt="AquaSens app" />
+        </div>
       </div>
     </div>
   );
